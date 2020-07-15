b2804d478cad030c3b2798efdc8284303fe4fbf9
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-07-15 14:29:45 +0700

    Create Common class to hold global variable

---
+++ app/src/main/java/com/rujirakongsomran/coverflowitemclick/Common/Common.java
@@ -0,0 +1,10 @@
+package com.rujirakongsomran.coverflowitemclick.Common;
+
+import com.rujirakongsomran.coverflowitemclick.Model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Common {
+    public static List<Movie> movieList = new ArrayList<>();
+}
